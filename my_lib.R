ab245e9fd44203e3d1f315831ae57c6998779b46
Paolo Bosetti <paolo.bosetti@unitn.it>
2024-10-07 13:11:36 +0200

    Lezione 20241007

---
+++ my_lib.R
@@ -0,0 +1,6 @@
+# In questo script definiamo funzioni di uso comune durante il corso.
+
+example_url <- function(example) {
+  url = paste0("https://paolobosetti.quarto.pub/data/", example)
+  return(url)
+}
